395bf044b2343be3f51f3a206fd8c80278f1bc5d
Александр Денисов <akatsuki_sanya@vk.com>
2024-07-31 11:51:33 +0500

    Add API models

--- backend/foodgram/api/models.py
+++ backend/foodgram/api/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+
+class Unit(models.Model):
+    title = models.CharField(max_length=30)
+
+
+class Ingredient(models.Model):
+    title = models.CharField(max_length=100)
+    unit = models.ForeignKey(Unit, on_delete=models.CASCADE)
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(unique=True)
+
+
+class RecipeIngredients(models.Model):
+    recipe = models.ForeignKey('Recipe', on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    amount = models.SmallIntegerField(default=0)
+
+
+class Recipe(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=256)
+    image = models.ImageField(upload_to='recipes/')
+    description = models.TextField(max_length=2000)
+    ingredients = models.ManyToManyField(
+        Ingredient,
+        through=RecipeIngredients
+    )
+    tags = models.ManyToManyField(Tag)
+    cooking_time = models.PositiveIntegerField()
+    in_favorites = models.BooleanField(default=False)
+    in_shop_list = models.BooleanField(default=False)
