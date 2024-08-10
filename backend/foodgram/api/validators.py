3ddfff4f503f0b833f1700a4a423b359798c23d0
Александр Денисов <akatsuki_sanya@vk.com>
2024-08-10 21:48:08 +0500

    ÑExpanded validate_ingredients func and Done all Post test in create Receip

--- backend/foodgram/api/validators.py
+++ backend/foodgram/api/validators.py
@@ -14,18 +14,18 @@ def validate_tags(tags_data):
         raise serializers.ValidationError(
             {'tags': 'Укажите теги.'}
         )
-    if (
-        len(tags_data) == 0
-        or len(set(tags_data)) < len(tags_data)
-    ):
+
+    if len(tags_data) == 0 or len(set(tags_data)) < len(tags_data):
         raise serializers.ValidationError(
             {'tags': 'Неверно указаны теги.'}
         )
+
     for tag in tags_data:
         if not Tag.objects.filter(id=tag).exists():
             raise serializers.ValidationError(
                 {'tags': 'Несуществующий тег.'}
             )
+
     return tags_data
 
 
@@ -40,4 +40,17 @@ def validate_ingredients(ingredient_list):
     if ingredient_list is None or len(ingredient_list) == 0:
         raise serializers.ValidationError(
             {'ingredients': 'Неверно переданы ингредиенты!'}
+        )
+
+    unique_ingredients = set()
+    for ingredient in ingredient_list:
+        unique_ingredients.add(ingredient['id'])
+        if ingredient['amount'] < 1:
+            raise serializers.ValidationError(
+                {'ingredients': 'Неверно передано кол-во ингредиентов!'}
+            )
+
+    if len(unique_ingredients) < len(ingredient_list):
+        raise serializers.ValidationError(
+            {'ingredients': 'Переданы одинаковые ингредиенты!'}
         )
