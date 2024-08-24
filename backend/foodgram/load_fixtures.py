3751766e5b7db303cff7fb86f0778cd1514c6191
Alexandr <akatsuki_sanya@vk.com>
2024-08-24 14:20:20 +0500

    Replace load func

---
+++ backend/foodgram/load_fixtures.py
@@ -0,0 +1,16 @@
+import json
+
+from django.core.management.base import BaseCommand
+from django.core.serializers import deserialize
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+        self.load_json('ingredients.json')
+
+    def load_json(self, file_path):
+        with open(file_path, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+            for obj in deserialize('json', json.dumps(data)):
+                obj.save()
+        self.stdout.write(self.style.SUCCESS('Successfully loaded JSON data'))
